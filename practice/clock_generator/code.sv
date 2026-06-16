66cc404b5ee9a642854e4cc80c5b567b2d9c13d5
waseem-vlsi <2025pvl0112@iitjammu.ac.in>
2026-06-16 19:09:40 +0530

    Create code.sv

---
+++ practice/clock_generator/code.sv
@@ -0,0 +1,25 @@
+module clk_gen (
+  input   logic        clk_in,
+
+  input   logic        reset,
+
+  output  logic        clk_v1,
+  output  logic        clk_v2
+);
+  logic q_out, d_in;
+
+  always_ff@(posedge clk_in or posedge reset) begin 
+    
+    if(reset) begin 
+     q_out <= 1'b0; 
+          end 
+    else begin 
+      q_out <= d_in;
+    end 
+  end
+  
+  assign d_in = ~q_out;
+  assign clk_v1 = clk_in & q_out;
+  assign clk_v2 = clk_v1 ^ clk_in;
+
+endmodule
